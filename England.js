3ec30baf00497d102006ced10c805e408175e5c1
Daniel Sládeček <83771369+Daniel1987Daniel@users.noreply.github.com>
2022-12-19 13:34:32 +0100

    Add files via upload

---
+++ England.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function England() {
+  return (
+    <div>
+      <nav className="NavEngland">
+        <h3><b>England</b></h3>
+        <img src="https://media.istockphoto.com/id/175600066/vector/badge-british-flag.jpg?s=612x612&w=0&k=20&c=C4C5TG9uWaU6vExJ_uyusUhHlmKe8GmVSUWfXBL9Zag=" alt="London-logo" className="logo-England" width={60}/>
+      </nav>
+      <hr></hr>
+      <img src="https://thumbs.dreamstime.com/b/westminster-bridge-palace-view-towards-parlament-elizabeth-tower-big-ben-london-uk-february-242023564.jpg" alt="London" width={220}/>
+      <p>
+        I spent twice my free time in Toronto city.
+      </p>
+    </div>
+  )
+};
---
+++ Toronto.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Toronto() {
+  return (
+    <div>
+      <nav className="NavEngland">
+        <h3><b>Toronto city</b></h3>
+        <img src="https://www.shutterstock.com/shutterstock/photos/1564556536/display_1500/stock-vector-toronto-real-estate-logo-design-vector-illustration-toronto-skyline-logo-maple-real-estate-logo-1564556536.jpg" alt="London-logo" className="logo-England" width={60}/>
+        </nav>
+        <hr></hr>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Toronto_-_ON_-_CN_Tower_bei_Nacht2.jpg/800px-Toronto_-_ON_-_CN_Tower_bei_Nacht2.jpg" alt="London" width={220} />
+        <p>
+          I spent twice my free time in Toronto city.
+          <br></br>
+          
+        </p>
+      <hr></hr>
+    </div>
+  )
+};
